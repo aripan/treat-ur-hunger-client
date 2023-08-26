6d44128184c053677f2645da6431171a58065654
Md Asaduzzaman Ripan <62987442+aripan@users.noreply.github.com>
2023-08-26 14:46:34 +0200

    implement shared section title for different sections

--- src/Pages/HomePage/Category/Category.jsx
+++ src/Pages/HomePage/Category/Category.jsx
@@ -12,51 +12,58 @@ import slide2 from "../../../assets/home/slide2.jpg";
 import slide3 from "../../../assets/home/slide3.jpg";
 import slide4 from "../../../assets/home/slide4.jpg";
 import slide5 from "../../../assets/home/slide5.jpg";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
 function Category() {
   return (
-    <Swiper
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={50}
-      slidesPerView={3}
-      navigation
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log("slide change")}
-      className="mb-20"
-    >
-      <SwiperSlide>
-        <img src={slide1} alt="slide1" />
-        <h3 className="mb-4 ml-16 -mt-16 text-4xl text-white uppercase">
-          Salads
-        </h3>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide2} alt="slide2" />
-        <h3 className="mb-4 ml-16 -mt-16 text-4xl text-white uppercase">
-          Pizza
-        </h3>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide3} alt="slide3" />
-        <h3 className="mb-4 ml-16 -mt-16 text-4xl text-white uppercase">
-          Soups
-        </h3>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide4} alt="slide4" />
-        <h3 className="mb-4 ml-16 -mt-16 text-4xl text-white uppercase">
-          Desserts
-        </h3>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide5} alt="slide5" />
-        <h3 className="mb-4 ml-16 -mt-16 text-4xl text-white uppercase">
-          Salads
-        </h3>
-      </SwiperSlide>
-    </Swiper>
+    <section>
+      <SectionTitle
+        heading={"Order online"}
+        subHeading={"From 11:00 am to 10:00 pm"}
+      ></SectionTitle>
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={50}
+        slidesPerView={3}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log("slide change")}
+        className="mb-20"
+      >
+        <SwiperSlide>
+          <img src={slide1} alt="slide1" />
+          <h3 className="mb-4 ml-16 -mt-16 text-4xl text-white uppercase">
+            Salads
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide2} alt="slide2" />
+          <h3 className="mb-4 ml-16 -mt-16 text-4xl text-white uppercase">
+            Pizza
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide3} alt="slide3" />
+          <h3 className="mb-4 ml-16 -mt-16 text-4xl text-white uppercase">
+            Soups
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide4} alt="slide4" />
+          <h3 className="mb-4 ml-16 -mt-16 text-4xl text-white uppercase">
+            Desserts
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide5} alt="slide5" />
+          <h3 className="mb-4 ml-16 -mt-16 text-4xl text-white uppercase">
+            Salads
+          </h3>
+        </SwiperSlide>
+      </Swiper>
+    </section>
   );
 }
 
