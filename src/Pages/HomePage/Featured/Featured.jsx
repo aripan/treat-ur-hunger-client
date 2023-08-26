a021d6dfd38f6afb41f7556e2ca3e4a8f5f70882
Md Asaduzzaman Ripan <62987442+aripan@users.noreply.github.com>
2023-08-26 17:55:30 +0200

    create parallax and add testimonials with react rating

--- src/Pages/HomePage/Featured/Featured.jsx
+++ src/Pages/HomePage/Featured/Featured.jsx
@@ -4,22 +4,28 @@ import "./Featured.css";
 
 const Featured = () => {
   return (
-    <div className="pt-8 my-20 text-white featured-image">
-      <SectionTitle heading={"Featured item"} subHeading={"check it out"} />
-      <div className="items-center justify-center pt-12 pb-20 px-36 md:flex">
-        <div>
-          <img src={featuredImage} alt="" />
-        </div>
-        <div className="md:ml-10">
-          <p>Aug 20, 2029</p>
-          <p className="uppercase">Where can i get some?</p>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-            veritatis laudantium, minima possimus quasi nam unde et nemo
-            officiis fuga enim doloremque ipsa numquam modi optio temporibus
-            soluta nostrum itaque!
-          </p>
-          <button className="btn btn-outline">Order Now</button>
+    <div className="bg-fixed featured-image">
+      <div className="pt-2 my-4 text-white feature-image-children ">
+        <SectionTitle heading={"Featured item"} subHeading={"check it out"} />
+        <div className="items-center justify-center pt-12 pb-20 px-36 md:flex">
+          <div className="flex-1 ">
+            <img className="rounded" src={featuredImage} alt="" />
+          </div>
+          <div className="flex-1 md:ml-10">
+            <p>Aug 20, 2029</p>
+            <br />
+            <p className="uppercase">Where can i get some?</p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+              veritatis laudantium, minima possimus quasi nam unde et nemo
+              officiis fuga enim doloremque ipsa numquam modi optio temporibus
+              soluta nostrum itaque!
+            </p>
+            <br />
+            <button className="text-white border-0 border-b-4 btn btn-outline">
+              Order Now
+            </button>
+          </div>
         </div>
       </div>
     </div>
